0348501ad91e203c6027840db8ed6560143a565a
Bikramjit Narwal <44352805+bikramjitnarwal@users.noreply.github.com>
2019-09-15 21:05:53 -0400

    Python Code

---
+++ customModel.py
@@ -0,0 +1,10 @@
+﻿import PIL
+
+from imageai.Prediction import ImagePrediction
+from imageai.Prediction.Custom import ModelTraining
+
+model_trainer = ModelTraining()
+
+model_trainer.setModelTypeAsSqueezeNet()
+model_trainer.setDataDirectory(r"C:\Users\Adrian\source\repos\just-your-feedback\color")
+model_trainer.trainModel(num_objects=2, num_experiments=20, enhance_data=False, batch_size=32, show_network_summary=True)
---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+requires = [
+    'pyramid==1.9.2',
+    'pyramid_jinja2==2.7',
+    'requests==2.20.0',
+    'waitress==1.1.0',
+    'tensorflow',
+    'opencv-python',
+    'keras',
+    'imageai'
+
+]
+
+setup(
+    name='webapp',
+    install_requires=requires,
+    entry_points="""\
+    [paste.app_factory]
+    main = webapp:main
+    """
+)
+
